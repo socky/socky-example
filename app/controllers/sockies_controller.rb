e716aa4b800f732faa1441e5084e764d8fde3ef2
Bernard Potocki <b.potocki@imanel.org>
2010-05-26 04:10:40 +0200

    Added notifications for all users about new user connecting

--- app/controllers/sockies_controller.rb
+++ app/controllers/sockies_controller.rb
@@ -3,10 +3,16 @@ class SockiesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def subscribe
+    render :socky do |page|
+      page.insert_html :top, 'messages', "<p>User #{h params[:client_id]} logged in</p>"
+    end
     render :text => "ok"
   end
 
   def unsubscribe
+    render :socky do |page|
+      page.insert_html :top, 'messages', "<p>User #{h params[:client_id]} logged out</p>"
+    end
     render :text => "ok"
   end
 
