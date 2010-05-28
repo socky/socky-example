e94fa1bf88525d6f4de8ba35db99dd52c11bdf79
Bernard Potocki <b.potocki@imanel.org>
2010-05-28 19:42:56 +0200

    Less cluttered demo

--- app/controllers/sockies_controller.rb
+++ app/controllers/sockies_controller.rb
@@ -17,18 +17,7 @@ class SockiesController < ApplicationController
   end
 
   def message
-    client_ids = params[:client_ids].to_s.split(",").collect(&:strip)
-    channels = params[:channels].to_s.split(",").collect(&:strip)
-    if !client_ids.empty? && !channels.empty?
-      type = :send_to_clients_on_channels
-    elsif !client_ids.empty?
-      type = :send_to_clients
-    elsif !channels.empty?
-      type = :send_to_channels
-    else
-      type = :send_to_all
-    end
-    render :socky => { :type => type, :client_ids => client_ids, :channels => channels } do |page|
+    render :socky => { :type => :send_to_all } do |page|
       page.insert_html :top, 'messages', "<p><b>#{h params[:current_user]}:</b> #{h params[:message]}</p>"
     end
     render :update do |page|
