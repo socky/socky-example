3a564ea5cd5de15ecc6c2edbf8349f5a022903ce
Bernard Potocki <b.potocki@imanel.org>
2010-08-01 01:55:36 +0200

    New syntax

--- app/controllers/sockies_controller.rb
+++ app/controllers/sockies_controller.rb
@@ -3,21 +3,21 @@ class SockiesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def subscribe
-    render :socky do |page|
+    Socky.send do |page|
       page.insert_html :top, 'messages', render(:partial => "login_message", :locals => {:user => params[:client_id]})
     end
     render :text => "ok"
   end
 
   def unsubscribe
-    render :socky do |page|
+    Socky.send do |page|
       page.insert_html :top, 'messages', render(:partial => "logout_message", :locals => {:user => params[:client_id]})
     end
     render :text => "ok"
   end
 
   def message
-    render :socky do |page|
+    Socky.send do |page|
       page.insert_html :top, 'messages', render(:partial => "message", :locals => {:user => params[:current_user], :message => params[:message]})
     end
     render :update do |page|
