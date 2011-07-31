021621c122ca94ee3df65cf1ec1c7ca70d40b203
Bernard Potocki <bernard.potocki@imanel.org>
2011-07-31 23:56:59 +0200

    Remove old javascript and add some comments

--- app/controllers/sockies_controller.rb
+++ app/controllers/sockies_controller.rb
@@ -3,6 +3,7 @@ require 'socky/authenticator'
 class SockiesController < ApplicationController
 
   def auth
+    # Authenticate user. If, for any reason, this method return status other that 200 then user will not be connected.
     result = Socky::Authenticator.authenticate(params, :allow_changing_rights => true, :secret => 'my_secret')
     render :json => result
   end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,32 +1,4 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
-// Event.observe(window, 'load', function() {
-//   Event.observe('message_form', 'submit', function(event) {
-//     $('message_form').request({
-//       method: 'post',
-//       onSuccess: function(t) {
-//         $('message').value = t.responseText;
-//       }
-//     });
-//     Event.stop(event); // stop the form from submitting
-//   });
-// });
-// 
-// $.message_counter = 0;
-// 
-// Socky.prototype.respond_to_message = function(msg) {
-//  data = JSON.parse(msg);
-//  var count = $.message_counter++;
-//  var message_id = 'message-' + count;
-//  Insertion.Bottom('messages', '<li style="opacity:0;" id="' + message_id + '" class="' + data[0] + '"><span>' + data[1] + '</span>' + data[2] + '</li>');
-//  new Effect.Opacity(message_id, { from: 0.0, to: 1.0, duration: 0.5 });
-//  var messages = document.getElementById("messages");
-//  messages.scrollTop = messages.scrollHeight;
-// }
-
-// NEW
-
+// Add message to chat
 function addMessage(type, header, content) {
   $("<li class='" + type + "'><span>" + header + "</span>" + content + "</li>").appendTo("#messages");
 };
@@ -35,10 +7,12 @@ function runChat(username) {
   // Connect to Socky Server
   var socky = new Socky.Client('ws://localhost:3001/websocket/my_app');
   
+  // Bind message after connecting to server
   socky.bind("socky:connection:established", function() {
     addMessage("system", '', "Connected - joining channel...");
   });
   
+  // Bind message after disconnecting from server
   socky.bind("socky:connection:closed", function() {
     addMessage("system", '', "Connection closed");
   });
@@ -50,14 +24,17 @@ function runChat(username) {
   // { data: { login: username } } all 'data' options are passed to other users
   channel = socky.subscribe("presence-chat-channel", { write: true, data: { login: username } });
   
+  // Bind message after successfull joining channel
   channel.bind("socky:subscribe:success", function(members) {
     addMessage("system", '', "Joined channel - " + members.length + " users online.");
   });
   
+  // Bind message after other user joins channel
   channel.bind("socky:member:added", function(data) {
     addMessage("login", "", data.login + " joined chat");
   });
   
+  // Bind message after other user disconnect from channel
   channel.bind("socky:member:removed", function(data) {
     addMessage("logout", "", data.login + " left chat");
   });
